46e9977b2f651cbd810e37a2e976c5befbda5713
Tuomas Tuhkanen <tuoppis@msn.com>
2021-12-08 14:24:01 +0200

    updates

--- src/components/ShowWord.js
+++ src/components/ShowWord.js
@@ -1,13 +1,19 @@
-function ShowWord({toGuessChars, guessedChars}) {
-    const checkGuessed = (ch) => {
-        return guessedChars.includes(ch) ? ch : "*";
+function ShowWord({toGuessChars, guessedChars, visible, reveal}) {
+    const makeCell = (ch, idx) => {
+        const hasChar = guessedChars.includes(ch);
+        const displayChar = reveal || hasChar ? ch : "*"
+        const endStyle = { backgroundColor: hasChar ? "#70ff7030" : "#ff707030", border: "none" }
+
+        return (
+            <td className="guessed" style={ reveal ? endStyle : {} }>{displayChar}</td>
+        );
     }
 
     return (
-        <table className="showWord">
+        <table id="showWord" style={{display:visible ? "block" : "none"}}>
             <thead>
                 <tr>
-                    {toGuessChars.map((ch, idx) => <td className="guessed">{checkGuessed(ch)}</td>)}
+                    {toGuessChars.map(makeCell)}
                 </tr>
             </thead>
         </table>
